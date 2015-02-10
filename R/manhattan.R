cd201a2652fe9c61889889c727a2acd08c10f54d
mcanouil <8896044+mcanouil@users.noreply.github.com>
2015-02-10 15:36:57 +0100

    Version 0.1.13
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- R/manhattan.R
+++ R/manhattan.R
@@ -56,55 +56,29 @@ manhattan <- function (data, chr, position, y, title = "Manhattan plot", xlab = 
     xBreaks <- data[whichIsCenter, "xPos"]
     p <- ggplot(data = data, aes_string(x = "xPos", y = y, colour = chr))
     if (bw) {
-        bw <- function (base_size = 12, base_family = "", noGrid = FALSE) {
+        blackwhite <- function (base_size = 12, base_family = "", noGrid = FALSE) {
             if (noGrid) {
-                noGridColour <- "transparent"
+                noGridColour <- c("transparent", "transparent") # "white"
             } else {
-                noGridColour <- c("gray90", "grey95")
+                noGridColour <- c("grey90", "grey98")
             }
-            theme(
-                line = element_line(colour = "black", size = 0.5, linetype = 1, lineend = "butt"),
-                rect = element_rect(fill = "white", colour = "black", size = 0.5, linetype = 1),
-                text = element_text(family = base_family, face = "plain", colour = "black", size = base_size, hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9),
-                axis.text = element_text(size = rel(0.8), colour = "black"),
-                strip.text = element_text(size = rel(0.8)),
-                axis.line = element_blank(),
-                axis.text.x = element_text(vjust = 1),
-                axis.text.y = element_text(hjust = 1),
-                axis.ticks = element_line(colour = "black"),
-                axis.title.x = element_text(),
-                axis.title.y = element_text(angle = 90),
-                axis.ticks.length = unit(0.15, "cm"),
-                axis.ticks.margin = unit(0.1, "cm"),
-                legend.background = element_rect(fill = "white", colour = "black"),
-                legend.margin = unit(0.2, "cm"),
-                legend.key = element_rect(fill = "white", colour = "black"),
-                legend.key.size = unit(1.2, "lines"),
-                legend.key.height = NULL,
-                legend.key.width = NULL,
-                legend.text = element_text(size = rel(0.8)),
-                legend.text.align = NULL,
-                legend.title = element_text(size = rel(0.8), face = "bold", hjust = 0),
-                legend.title.align = NULL,
-                legend.position = "right",
-                legend.direction = NULL,
-                legend.justification = "center",
-                legend.box = NULL,
-                panel.background = element_rect(fill = "white", colour = "black"),
-                panel.border = element_blank(),
-                panel.grid.major = element_line(colour = noGridColour[1]),
-                panel.grid.minor = element_line(colour = noGridColour[length(noGridColour)], size = 0.25),
-                panel.margin = unit(0.25, "lines"),
-                strip.background = element_rect(fill = "black", colour = "black"),
-                strip.text.x = element_text(colour = "white"),
-                strip.text.y = element_text(angle = -90, colour = "white"),
-                plot.background = element_rect(colour = "white"),
-                plot.title = element_text(size = rel(1.2)),
-                plot.margin = unit(c(1, 1, 0.5, 0.5), "lines"),
-                complete = TRUE
-            )
+            theme_grey(base_size = base_size, base_family = base_family) %+replace%
+                theme(
+                    axis.text = element_text(size = rel(0.8)),
+                    axis.ticks = element_line(colour = "black"),
+                    legend.background = element_rect(fill = "white", colour = "black"),
+                    legend.key = element_rect(fill = "white", colour = "black"),
+                    legend.position = "right",
+                    legend.justification = "center",
+                    legend.box = NULL,
+                    panel.background = element_rect(fill = "white", colour = NA),
+                    panel.border = element_rect(fill = NA, colour = "black"),
+                    panel.grid.major = element_line(colour = noGridColour[1], size = 0.2),
+                    panel.grid.minor = element_line(colour = noGridColour[2], size = 0.5),
+                    strip.background = element_rect(fill = "grey80", colour = "black", size = 0.2)
+                )
         }
-        p <- p + bw(base_size = base_size, noGrid = noGrid)
+        p <- p + blackwhite(base_size = base_size, noGrid = noGrid)
     } else {}
     p <- p + theme(panel.background = element_rect(colour = "black"), panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(), legend.position = "none")
     p <- p + geom_point(size = 1.5, shape = 1, na.rm = TRUE)
