968c93f3f82bc0a5f86ac3493c0c767757e70b96
mcanouil <8896044+mcanouil@users.noreply.github.com>
2015-09-29 11:49:34 +0200

    Version 0.1.19
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- R/qqplot.R
+++ R/qqplot.R
@@ -21,15 +21,25 @@ qqplot <- function (pvalue, lambdaNames = NULL, pt.size = 1, bw = TRUE, noGrid =
         } else {}
         return(c("black", hcl(h = (seq(h[1], h[2], length = (n-1))), c = 100, l = 65)))
     }
-    if (is.null(lambdaNames)) {
-        lambdaNames <- colnames(pvalue)
+    if (is.null(lambdaNames) & any(colnames(pvalue)=="")) {
+        lambdaNames <- paste0("lambda", seq(ncol(pvalue)))
     } else {}
 
-    obsmax <- NA
-    expmax <- NA
-    labs <- NULL
-    res <- NULL
-    for (i in seq(ncol(pvalue))) {
+    # labs <- NULL
+    # res <- NULL
+    # for (i in seq(ncol(pvalue))) {
+        # pv <- pvalue[, i]
+        # X2 <- qnorm(pv, lower.tail = FALSE)^2
+        # gc <- median(X2, na.rm = TRUE)/qchisq(0.5, df = 1)
+        # obspval <- sort(pv)
+        # logobspval <- -(log10(obspval))
+        # exppval <- c(1:length(obspval))
+        # logexppval <- -(log10((exppval-0.5)/length(exppval)))
+        # labs <- c(labs, bquote(lambda[gc]^.(lambdaNames[i]) == .(round(gc, 4))))
+        # res <- rbind(res, data.frame(logexppval, logobspval, i))
+    # }
+
+    res <- do.call("rbind", mclapply(seq(ncol(pvalue)), mc.cores = detectCores(), function (i) {
         pv <- pvalue[, i]
         X2 <- qnorm(pv, lower.tail = FALSE)^2
         gc <- median(X2, na.rm = TRUE)/qchisq(0.5, df = 1)
@@ -37,18 +47,14 @@ qqplot <- function (pvalue, lambdaNames = NULL, pt.size = 1, bw = TRUE, noGrid =
         logobspval <- -(log10(obspval))
         exppval <- c(1:length(obspval))
         logexppval <- -(log10((exppval-0.5)/length(exppval)))
-
-        obsmax <- max(obsmax, trunc(max(logobspval, na.rm = TRUE))+1, na.rm = TRUE)
-        expmax <- max(expmax, trunc(max(logexppval, na.rm = TRUE))+1, na.rm = TRUE)
-        labs <- c(labs, bquote(lambda[gc]^.(lambdaNames[i]) == .(round(gc, 4))))
-        res <- rbind(res, data.frame(logexppval, logobspval, i))
-    }
-
-    res[, "i"] <- factor(res[, "i"], levels = seq(ncol(pvalue)))
-    whichInfinite <- apply(res, 1, function (iRow) {
-        return(any(sapply(as.numeric(iRow[-3]), is.infinite)))
-    })
-    res <- res[!whichInfinite, ]
+        # labs <- bquote(lambda[gc]^.(lambdaNames[i]) == .(round(gc, 4)))
+        labs <- paste0(lambdaNames[i], "=", round(gc, 4))
+        tmp <- data.frame(logexppval, logobspval, i, labs)
+        whichInfinite <- apply(res, 1, function (iRow) {
+            return(any(sapply(as.numeric(iRow[-3]), is.infinite)))
+        })
+        return(tmp[!whichInfinite, ])
+    }))
     p <- ggplot(data = res)
     if (bw) {
         blackwhite <- function (base_size = 12, base_family = "", noGrid = FALSE) {
@@ -75,8 +81,8 @@ qqplot <- function (pvalue, lambdaNames = NULL, pt.size = 1, bw = TRUE, noGrid =
         }
         p <- p + blackwhite(base_size = base_size, noGrid = noGrid)
     } else {}
-    p <- p + geom_point(aes_string(x = "logexppval", y = "logobspval", colour = "i"), size = pt.size)
     p <- p + geom_abline(intercept = 0, slope = 1)
+    p <- p + geom_point(aes_string(x = "logexppval", y = "logobspval", colour = "i"), size = pt.size, shape = 1)
     p <- p + labs(x = bquote(Expected -log[10](P[value])), y = bquote(Observed -log[10](P[value])))
     if (ncol(pvalue) > length(c("dodgerblue", "firebrick2", "springgreen3", "maroon2", "goldenrod2", "deepskyblue"))) {
         p <- p + scale_colour_manual(name = element_blank(), breaks = seq(ncol(pvalue)), labels = labs, values = .ggplotColours(ncol(pvalue)))
@@ -84,12 +90,9 @@ qqplot <- function (pvalue, lambdaNames = NULL, pt.size = 1, bw = TRUE, noGrid =
         p <- p + scale_colour_manual(name = element_blank(), breaks = seq(ncol(pvalue)), labels = labs, values =  c("dodgerblue", "firebrick2", "springgreen3", "maroon2", "goldenrod2", "deepskyblue"))
     }
     p <- p + labs(title = "Q-Q plot") + theme(plot.title = element_text(lineheight = 0.8, face = "bold"))
-    axisLim <- c(0, max(p$data[, as.character(p$layers[[1]]$mapping[c(1, 2)])]))
+    axisLim <- range(pretty_breaks()(range(unlist(mclapply(seq(ncol(pvalue)), mc.cores = detectCores(), function (i) {
+        range(res[res[, "i"]%in%i, c(1, 2)])
+    })))))
     p <- p + xlim(axisLim) + ylim(axisLim)
-    if (which.max(apply(p$data[, c("logexppval", "logobspval")], 2, max))==2) {
-         p <- p + theme(legend.justification = c(1, 0), legend.position = c(1, 0))
-    } else {
-        p <- p + theme(legend.justification = c(0, 1), legend.position = c(0, 1))
-    }
     return(p)
 }
